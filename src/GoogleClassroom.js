7d65c6d74732dbdb7a2cab682de189297080995b
Jason <kunfz96@gmail.com>
2020-10-12 23:21:39 -0400

    [#3] embed youtube video without overlay

--- src/GoogleClassroom.js
+++ src/GoogleClassroom.js
@@ -1,5 +1,22 @@
-window.addEventListener('load', function() {
-    console.log('DOM is loaded')
-    // document.getElementById("p").style.background = "green"
-})
-console.log("GG")
+function hijackYoutubeLinkClicks(e) {
+    console.log("Detected a click");
+    let target = e.target;
+    // Interruption Criteria, <a> tags with href containing 'youtube'
+    if (target.nodeName === "A") {
+        let destination = target.getAttribute("href");
+        if (destination.includes("youtube")) {
+            console.log("Stopped redirection to Youtube");
+            e.preventDefault();
+
+            // Embed the Video
+            let formattedUrl = destination.replace("youtube.com/watch?v=", "youtube.com/embed/");
+            target.outerHTML += `<iframe width='420' height='315' src='${formattedUrl}?autoplay=1'></iframe>`;
+        }
+    }
+}
+
+window.addEventListener('load', function (e) {
+    console.log("Page Loaded");
+    window.addEventListener('click', hijackYoutubeLinkClicks);
+});
+
