0048a17087595ebe3439bab111dde27859250d5f
nwan3 <nwan3@gatech.edu>
2020-10-18 13:57:15 -0400

    added mock-up student tab, ranking tab, and store tab
    added empty line to the end of several files

--- src/MinervaMenu.js
+++ src/MinervaMenu.js
@@ -1,13 +1,23 @@
-let studentNameField = document.getElementById('studentName');
-
-studentNameField.onkeydown  = function(event) {
-	if (event.code == "Enter" || event.key == "Enter") {
-		chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-    		var activeTab = tabs[0];
-			console.log(studentNameField.value);
-			console.log(tabs);
-			chrome.tabs.sendMessage(activeTab.id, {"message": studentNameField.value});
-		});
+window.onload = function() {
+	console.log("onloading");
+	let studentNameField = document.getElementById('studentName');
+	studentNameField.onkeydown  = function(event) {
+		if (event.code == "Enter" || event.key == "Enter") {
+			chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+	    		var activeTab = tabs[0];
+				console.log(studentNameField.value);
+				console.log(tabs);
+				chrome.tabs.sendMessage(activeTab.id, {"message": studentNameField.value});
+			});
+		}
 	}
-}
 
+	let images = ["golden_star.jpg", "pencil.jpg", "ruler.jpg"]
+	for (var i = 1; i <= 3; i++) {
+		let image = document.getElementById(`item-${i}-image`);
+		var imgURL = chrome.extension.getURL(`images/${images[i - 1]}`);
+		console.log(imgURL)
+		image.src = imgURL;
+	}
+
+}
