97ea12d5087a032a79d632b00a9da6526be03b36
nwan3 <nwan3@gatech.edu>
2020-10-23 18:39:27 -0400

    added messaging to notify GoogleClassroom that student name was entered. Allowed for inline scripts
    changed var to let

--- src/MinervaMenu.js
+++ src/MinervaMenu.js
@@ -1,23 +1,26 @@
 window.onload = function() {
-	console.log("onloading");
-	let studentNameField = document.getElementById('studentName');
-	studentNameField.onkeydown  = function(event) {
-		if (event.code == "Enter" || event.key == "Enter") {
-			chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-	    		var activeTab = tabs[0];
-				console.log(studentNameField.value);
-				console.log(tabs);
-				chrome.tabs.sendMessage(activeTab.id, {"message": studentNameField.value});
-			});
-		}
-	}
+    console.log("onloading");
+    let studentNameField = document.getElementById('studentName');
+    studentNameField.onkeydown  = function(event) {
+        if (event.code == "Enter" || event.key == "Enter") {
+            chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+                var activeTab = tabs[0];
+                console.log(studentNameField.value);
+                // chrome.tabs.sendMessage(activeTab.id, {"message": studentNameField.value});
+                chrome.storage.local.set({"studentName": studentNameField.value}, function() {
+                    console.log('Value is set to ' + studentNameField.value);
+                    chrome.storage.local.get(console.log)
+                });
+            });
+        }
+    }
 
-	let images = ["golden_star.jpg", "pencil.jpg", "ruler.jpg"]
-	for (var i = 1; i <= 3; i++) {
-		let image = document.getElementById(`item-${i}-image`);
-		var imgURL = chrome.extension.getURL(`images/${images[i - 1]}`);
-		console.log(imgURL)
-		image.src = imgURL;
-	}
+    let images = ["golden_star.jpg", "pencil.jpg", "ruler.jpg"]
+    for (var i = 1; i <= 3; i++) {
+        let image = document.getElementById(`item-${i}-image`);
+        let imgURL = chrome.extension.getURL(`images/${images[i - 1]}`);
+        console.log(imgURL)
+        image.src = imgURL;
+    }
 
 }
--- src/background.js
+++ src/background.js
@@ -1,11 +1,10 @@
 chrome.runtime.onInstalled.addListener(function() {
-
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-		  conditions: [new chrome.declarativeContent.PageStateMatcher ({
-		    pageUrl: { hostEquals: 'classroom.google.com'},
-		  })],
-		  actions: [new chrome.declarativeContent.ShowPageAction()]
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    	chrome.declarativeContent.onPageChanged.addRules([{
+		  	conditions: [new chrome.declarativeContent.PageStateMatcher ({
+		    	pageUrl: { hostEquals: 'classroom.google.com'},
+		  	})],
+		  	actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
   });
 });
