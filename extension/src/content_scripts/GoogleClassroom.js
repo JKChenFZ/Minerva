5a69530126dd3f671c06c5e08bc8576ba79fd8ca
Jason <kunfz96@gmail.com>
2020-10-24 22:52:08 -0400

    [#26] Separation of scripts

--- extension/src/content_scripts/GoogleClassroom.js
+++ extension/src/content_scripts/GoogleClassroom.js
@@ -1,60 +1,25 @@
-const EXIT_BUTTON_SVG = "<svg aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"times-circle\" class=\"svg-inline--fa fa-times-circle fa-w-16\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path fill=\"currentColor\" d=\"M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z\"></path></svg>";
+const GOOGLE_CLASSROOM_OVERLAY_SCRIPT = "src/embed_scripts/GoogleClassroomOverlay.js";
+const FONT_AWESOME_URL = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css";
 
-function exitButtonOnclick(e) {
-    if (e.target.nodeName != "path" && e.target.nodeName != "svg") {
-        return;
-    }
+function addScript(scriptPath) {
+    let script = document.createElement("script");
+    script.src = chrome.runtime.getURL(scriptPath);
 
-    let overlay = document.getElementById("studyModeLocker");
-    if (overlay) {
-        overlay.remove();
-        // Add the handler back
-        window.addEventListener("click", hijackYoutubeLinkClicks);
-    }
+    (document.head || document.documentElement).appendChild(script);
 }
 
-function hijackYoutubeLinkClicks(e) {
-    console.log("Detected a click");
+function addStyleSheet(url) {
+    // Add the style sheet
+    let style = document.createElement("link");
+    style.rel = "stylesheet";
+    style.type = "text/css";
+    style.href = url;
+    style.crossOrigin = "anonymous";
 
-    let target = e.target;
-    // Interruption Criteria, <a> tags with href containing 'youtube'
-    if (target.nodeName === "A") {
-        let destination = target.getAttribute("href");
-        if (destination.includes("youtube")) {
-            e.preventDefault();
-            console.log("Stopped redirection to Youtube");
-
-            // Remove click event listener
-            window.removeEventListener("click", hijackYoutubeLinkClicks);
-
-            // Overlay
-            let overlay = document.createElement("div");
-            overlay.id = "studyModeLocker";
-
-            // Add overlay to the body
-            let siteBody = document.getElementsByTagName("BODY")[0];
-            siteBody.appendChild(overlay);
-
-            // Embed the exit button
-            let exitButton = document.createElement("div");
-            exitButton.id = "exitButton";
-            exitButton.innerHTML += EXIT_BUTTON_SVG;
-            overlay.appendChild(exitButton);
-
-            // Register handler for the exit button clicks.
-            // I was unable to add the handler on the 'path' tag above programmatically :(
-            window.addEventListener("click", exitButtonOnclick);
-
-            // Embed the Video
-            let formattedUrl = destination.replace("youtube.com/watch?v=", "youtube.com/embed/");
-            overlay.innerHTML += `<div id=videoWrapper><iframe width=100% height=100% src='${formattedUrl}'></iframe><div>`;
-        }
-    }
+    (document.head||document.documentElement).appendChild(style);
+    console.log(`Added stylesheet from ${url}`);
 }
 
-// eslint-disable-next-line no-unused-vars
-window.addEventListener("load", (_) => {
-    window.addEventListener("click", hijackYoutubeLinkClicks);
-    console.log("Google Classroom Handler registered");
-});
-
+// This content script is guaranteed to fire after "load" event is fired
+addStyleSheet(FONT_AWESOME_URL);
+addScript(GOOGLE_CLASSROOM_OVERLAY_SCRIPT);
---
+++ extension/src/embed_scripts/GoogleClassroomOverlay.js
@@ -0,0 +1,85 @@
+function exitButtonOnclick() {
+    let overlay = document.getElementById("studyModeLocker");
+    if (overlay) {
+        overlay.remove();
+        // Add the handler back
+        window.addEventListener("click", hijackYoutubeLinkClicks);
+    }
+}
+
+function addControlButtons() {
+    let overlay = document.getElementById("studyModeLocker");
+    
+    // Create nodes first
+    let exitButton = document.createElement("BUTTON");
+    let exitIcon = document.createElement("I");
+
+    // Program the button logic
+    exitIcon.setAttribute("class", "far fa-times-circle icon-left-padding");
+    exitButton.setAttribute("class", "button");
+    exitButton.innerText = "Leave";
+    exitButton.addEventListener("click", (e) => {
+        e.stopPropagation();
+        exitButtonOnclick();
+    });
+
+    exitButton.appendChild(exitIcon);
+    overlay.appendChild(exitButton);
+}
+
+function addYoutubeIFrame(rawDestination) {
+    let formattedUrl = rawDestination.replace("youtube.com/watch?v=", "youtube.com/embed/");
+    let overlay = document.getElementById("studyModeLocker");
+
+    // Create nodes first
+    let wrapperDiv = document.createElement("div");
+    let iframe = document.createElement("iframe");
+
+    wrapperDiv.id = "videoWrapper";
+    iframe.setAttribute("width", "100%");
+    iframe.setAttribute("height", "100%");
+    iframe.src = formattedUrl;
+
+    wrapperDiv.appendChild(iframe);
+    overlay.appendChild(wrapperDiv);
+}
+
+function hijackYoutubeLinkClicks(e) {
+    console.log("Detected a click");
+
+    let target = e.target;
+    // Interruption Criteria, <a> tags with href containing 'youtube'
+    if (target.nodeName === "A") {
+        let destination = target.getAttribute("href");
+        if (destination.includes("youtube")) {
+            e.preventDefault();
+            console.log("Stopped redirection to Youtube");
+
+            // Remove click event listener
+            window.removeEventListener("click", hijackYoutubeLinkClicks);
+
+            // Create overlay element
+            let overlay = document.createElement("div");
+            overlay.id = "studyModeLocker";
+            overlay.addEventListener("click", (e) => {
+                e.stopPropagation();
+                console.log("Overlay clicked");
+            });
+
+            // Add overlay to the body
+            let siteBody = document.getElementsByTagName("BODY")[0];
+            siteBody.appendChild(overlay);
+
+            // Populate content on the overlay
+            // Embed the exit button
+            addControlButtons();
+
+            // Embed the Video
+            addYoutubeIFrame(destination);
+        }
+    }
+}
+
+// This file is guaranteed to be injected after "load" event is fired
+window.addEventListener("click", hijackYoutubeLinkClicks);
+console.log("Google Classroom Overlay registered");
