299e898dcad803d5c0fd6f2334b1be18dc79e063
Jason <kunfz96@gmail.com>
2020-10-27 13:52:06 -0400

    added linter to backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
+app.get("/", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({ status: true }));
 });
 
 app.listen(port, () => {
-    console.log(`Minerva backend is active at http://localhost:${port}`)
+    console.log(`Minerva backend is active at http://localhost:${port}`);
 });
