09c1d07cda6596810d7c788f5768ade8f8520c9c
Jason <kunfz96@gmail.com>
2020-11-01 17:00:32 -0500

    Migration Checkpoint

---
+++ extension/src/content_scripts/google_classroom/OverlayFacialDetection.js
@@ -0,0 +1,13 @@
+// Add the webcam feed
+async function integrateWebcam() {
+    // setupWebcam();
+    // await enableCamera();
+
+    // canvas = document.getElementById("overlayVideoCanvas");
+    // ctx = canvas.getContext("2d");
+    // ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+
+    // renderPrediction();
+}
+
+export { integrateWebcam };
--- extension/src/embed_scripts/README.md
+++
@@ -1,2 +0,0 @@
-## Note
-- Scripts in this folder are meant to be injected into the target web page directly and executed in the same scope as the web page
