f79afa2120a2b4e44314b52b3e2731cef9dddb98
MrNickWan <daomingwan@gmail.com>
2020-11-02 14:20:14 -0500

    30 code review

--- extension/src/InstructorDashboard/InstructorDashboard.js
+++ extension/src/InstructorDashboard/InstructorDashboard.js
@@ -1,7 +1,7 @@
 let activeFeedback = [{x: new Date(0,0,0,0,1,2,0), y: "2", students: ["Yuki","John"]},{x: new Date(0,0,0,0,20,1,0), y: "3", students: ["John","Jack", "Seth"]}];
 let passiveFeedback = [{x: new Date(0,0,0,0,12,45,0), y: "1", students: ["Seth"] }, {x: new Date(0,0,0,1,14,5,0), y: "2", students: ["Yuki","John"] }];
 let data = [activeFeedback, passiveFeedback];
-displayFormatFunction = function(label, index, labels) {
+function displayFormatFunction(label, index, labels) {
     let hoursIndex = label.indexOf(":");
     if (label.substring(0, hoursIndex) == "12") {
         return label.substring(hoursIndex + 1);
@@ -9,15 +9,14 @@ displayFormatFunction = function(label, index, labels) {
         return label;
     }
 }
-displayStudents = function (TooltipItem, object) {
+function displayStudents(TooltipItem, object) {
     let label = [];
     let index = TooltipItem[0].index;
     let datasetIndex = TooltipItem[0].datasetIndex;
     for (let i = 0; i < data[datasetIndex][index].students.length; i++) {
         label.push(data[datasetIndex][index].students[i]);
     }
-    return label.join(" ");
-    
+    return label.join(" ");   
 }
 window.onload = function() {
     let ctx = document.getElementById("myChart").getContext("2d");
